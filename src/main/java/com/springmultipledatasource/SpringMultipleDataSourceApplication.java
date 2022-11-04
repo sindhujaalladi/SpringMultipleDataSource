8ffc31bacf27a0ac5de637b26af2de7370c51ff3
sindhujaalladi <sindhu.alladi1993@gmail.com>
2022-11-04 16:41:54 -0500

    inital commit

---
+++ src/main/java/com/springmultipledatasource/SpringMultipleDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.springmultipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultipleDataSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultipleDataSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmultipledatasource/SpringMultipleDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmultipledatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultipleDataSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
